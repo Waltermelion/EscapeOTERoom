4d427b55d32d7dd9edbe7db864e050c2cdc78a56
Waltermelion <guilhermelpmeneses@gmail.com>
2023-11-21 22:07:11 +0000

    (UPDATE) Left e right camara movement

--- Assets/2-Scripts/Camara controler.cs
+++ Assets/2-Scripts/Camara controler.cs
@@ -1,25 +1,51 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class Camaracontroler : MonoBehaviour
 {
     [SerializeField] private GameObject mainCameraObject;
-    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
 
 
     public void RotateLeft()
     {
+        StartCoroutine(RotateCameraNegative90Degrees());
+        //Quaternion targetRotation = Quaternion.Euler(0, transform.eulerAngles.y - 90, 0);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
         //mainCameraObject.transform.Rotate(0, Time.deltaTime * (-speed), 0);
 
         //mainCameraObject.transform.rotation = new Quaternion(Mathf.Lerp(0,90,(Time.deltaTime)),0,0,0);
 
-        mainCameraObject.transform.DORotate(new Vector3(0,mainCameraObject.transform.rotation.y - 90, 0), 1f);
+        //mainCameraObject.transform.DORotate(new Vector3(0,mainCameraObject.transform.rotation.y - 90, 0), 1f);
         
     }
 
     public void RotateRight()
     {
-        mainCameraObject.transform.DORotate(new Vector3(0, mainCameraObject.transform.rotation.y + 90, 0), 1f);
+        StartCoroutine(RotateCamera90Degrees());
+        //Quaternion targetRotation = Quaternion.Euler(0, transform.eulerAngles.y + 90, 0);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //mainCameraObject.transform.DORotate(new Vector3(0, mainCameraObject.transform.rotation.y + 90, 0), 1f);
+    }
+    IEnumerator RotateCamera90Degrees()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0, transform.eulerAngles.y + 90, 0);
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateCameraNegative90Degrees()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0, transform.eulerAngles.y - 90, 0);
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
