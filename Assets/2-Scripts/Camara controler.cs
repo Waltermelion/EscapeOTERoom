dcfd66ab94c84a05adaf2d79a70a4d251f4b616b
Waltermelion <guilhermelpmeneses@gmail.com>
2023-11-21 23:14:15 +0000

    (NEW+UPDATE)script camara e gestos

--- Assets/2-Scripts/Camara controler.cs
+++ Assets/2-Scripts/Camara controler.cs
@@ -4,30 +4,19 @@ using UnityEngine;
 
 public class Camaracontroler : MonoBehaviour
 {
-    [SerializeField] private GameObject mainCameraObject;
+    //Velocidade de rotação da camara;
     [SerializeField] private float rotationSpeed;
 
-
+    //É chamado quando o jogador faz o gesto de rodar para a esquerda
     public void RotateLeft()
     {
-        StartCoroutine(RotateCameraNegative90Degrees());
-        //Quaternion targetRotation = Quaternion.Euler(0, transform.eulerAngles.y - 90, 0);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-        //mainCameraObject.transform.Rotate(0, Time.deltaTime * (-speed), 0);
-
-        //mainCameraObject.transform.rotation = new Quaternion(Mathf.Lerp(0,90,(Time.deltaTime)),0,0,0);
-
-        //mainCameraObject.transform.DORotate(new Vector3(0,mainCameraObject.transform.rotation.y - 90, 0), 1f);
-        
+        StartCoroutine(RotateCameraNegative90Degrees());        
     }
 
+    //É chamado quando o jogador faz o gesto de rodar para a direita
     public void RotateRight()
     {
-        StartCoroutine(RotateCamera90Degrees());
-        //Quaternion targetRotation = Quaternion.Euler(0, transform.eulerAngles.y + 90, 0);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        //mainCameraObject.transform.DORotate(new Vector3(0, mainCameraObject.transform.rotation.y + 90, 0), 1f);
+        StartCoroutine(RotateCamera90Degrees());        
     }
     IEnumerator RotateCamera90Degrees()
     {
