9111020a0bdad446c7600617cef894207b87c470
Waltermelion <guilhermelpmeneses@gmail.com>
2023-11-24 03:12:47 +0000

    contour hands and lines

--- Assets/2-Scripts/HandFinder.cs
+++ Assets/2-Scripts/HandFinder.cs
@@ -6,17 +6,50 @@ using UnityEngine;
 
 public class HandFinder : WebCamera
 {
+    [SerializeField] private FlipMode imageFlip;
+    [SerializeField] private float threshold = 96.4f;
+    [SerializeField] private bool showProcessingImage = true;
+    [SerializeField] private float CurveAccuracy = 10f;
+    [SerializeField] private float MinArea = 5000f;
+
     private Mat image;
+    private Mat processImage = new Mat();
+    private Point[][] contours;
+    private HierarchyIndex[] hierachy;
     protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
     {
         image = OpenCvSharp.Unity.TextureToMat(input);
 
+        Cv2.Flip(image, image, imageFlip);
+        Cv2.CvtColor(image, processImage, ColorConversionCodes.BGR2GRAY);
+        Cv2.Threshold(processImage, processImage, threshold, 255, ThresholdTypes.BinaryInv);
+        Cv2.FindContours(processImage, out contours,out hierachy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple, null);
+
+        foreach (Point[] contour in contours)
+        {
+            Point[] points = Cv2.ApproxPolyDP(contour, CurveAccuracy, true);
+            var area = Cv2.ContourArea(contour);
+
+            if (area > MinArea)
+            {
+                drawContour(processImage, new Scalar(127, 127, 127), 2, points);
+            }
+        }
 
         if (output == null)        
-            output = OpenCvSharp.Unity.MatToTexture(image);        
+            output = OpenCvSharp.Unity.MatToTexture(showProcessingImage ? processImage : image);        
         else        
-            OpenCvSharp.Unity.MatToTexture(image, output);        
+            OpenCvSharp.Unity.MatToTexture(showProcessingImage ? processImage : image, output);        
 
         return true;
+    }
+
+    private void drawContour(Mat Image, Scalar Color, int Thickness, Point[] Points)
+    {
+        for(int i = 1;i< Points.Length; i++)
+        {
+            Cv2.Line(Image, Points[i - 1], Points[i], Color, Thickness);
+        }
+        Cv2.Line(Image, Points[Points.Length - 1], Points[0], Color, Thickness);
     }
 }
