3eef77726f401d97071bda9ee477f6b5e14f620b
Diogo <53532032+Di0go@users.noreply.github.com>
2023-12-03 00:52:54 +0000

    Puzzle3

--- Assets/2-Scripts/Puzzle3.cs
+++ Assets/2-Scripts/Puzzle3.cs
@@ -4,15 +4,122 @@ using UnityEngine;
 
 public class Puzzle3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 10;
+    public HandInputHandler handInputHandler;
+    public bool[] slots;
+    public GameObject[] slotsGmo;
+    public Vector3[] slotRotations;
+    public int currentSlot = 0;
+    public int currentRotation = 0;
+    public bool slotCanRotate = true;
+    private Quaternion targetRotation;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (slotCanRotate)
+        {
+            if (handInputHandler.camControler.isInPuzzle && handInputHandler.currentPuzzle == 2)
+            {
+                RotateObject();
+            }
+        }
+        CheckSlotRotation();
+    }
+    void CheckSlotRotation()
+    {
+        if (currentSlot == 1 && currentRotation != 2)
+        {
+            slots[1] = false;
+        }
+        else if(currentSlot == 1)
+        {
+            slots[1] = true;
+        }
+        if (currentSlot == 0 && currentRotation != 3)
+        {
+            slots[0] = false;
+        }
+        else if(currentSlot == 0)
+        {
+            slots[0] = true;
+        }
+
+
+
+        if (slots[0] == true && slots[1] == true)
+        {
+            // Open Drawer Animation
+        }
+    }
+
+    void RotateObject()
+    {
+        if (handInputHandler.hDetectedState == 1f)//1 is Left
+        {
+            StartCoroutine(RotateSlot90Degrees());
+        }
+        else if (handInputHandler.hDetectedState == 2f)//2 is right
+        {
+            StartCoroutine(RotateSlotNegative90());
+        }
+        else if (handInputHandler.hDetectedState == 4f)
+        {
+            if (currentSlot == slotsGmo.Length - 1)
+            {
+                currentSlot = 0;
+            }
+            else
+            {
+                currentSlot += 1;
+            }            
+        }
+
+        if (handInputHandler.hDetectedState != 0)
+        {
+            handInputHandler.hDetectedState = 0;
+        }
+    }
+    IEnumerator RotateSlot90Degrees()
+    {
+        if (currentRotation == 0)
+        {
+            currentRotation = slotRotations.Length - 1;
+            targetRotation = Quaternion.Euler(slotRotations[currentRotation]);
+        }
+        else
+        {
+            currentRotation -= 1;
+            targetRotation = Quaternion.Euler(slotRotations[currentRotation]);           
+        }
+        slotCanRotate = false;
+        while (Quaternion.Angle(slotsGmo[currentSlot].transform.rotation, targetRotation) > 0.01f)
+        {
+            slotsGmo[currentSlot].transform.rotation = Quaternion.Slerp(slotsGmo[currentSlot].transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+        slotsGmo[currentSlot].transform.rotation = targetRotation;
+        slotCanRotate = true;
+    }
+
+    IEnumerator RotateSlotNegative90()
+    {
+        if (currentRotation == slotRotations.Length - 1)
+        {
+            currentRotation = 0;
+            targetRotation = Quaternion.Euler(slotRotations[currentRotation]);
+        }
+        else
+        {
+            currentRotation += 1;
+            targetRotation = Quaternion.Euler(slotRotations[currentRotation]);
+        }
+        slotCanRotate = false;
+        while (Quaternion.Angle(slotsGmo[currentSlot].transform.rotation, targetRotation) > 0.01f)
+        {
+            slotsGmo[currentSlot].transform.rotation = Quaternion.Slerp(slotsGmo[currentSlot].transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+        slotsGmo[currentSlot].transform.rotation = targetRotation;
+        slotCanRotate = true;
     }
 }
